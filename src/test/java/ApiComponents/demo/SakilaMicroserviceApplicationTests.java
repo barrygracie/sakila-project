a53b7882ed37ec080b3a38b95a78933938a33bdc
Barry Gracie <bgracie@hotmail.co.uk>
2022-10-20 11:56:55 +0100

    initial upload

---
+++ src/test/java/ApiComponents/demo/SakilaMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package ApiComponents.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SakilaMicroserviceApplicationTests {
+/*
+	@Test
+	void contextLoads() {
+	}
+*/
+}
